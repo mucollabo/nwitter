6275014aee9898936854c3e86ca69f93157b1a9e
Charles An <mucollabo@gmail.com>
2020-09-20 22:36:33 +0900

    study 1.0

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+
+import * as firebase from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC0Vlut07MqjGgkXcxjrS0uCjv-c9f9GLs",
+    authDomain: "nwitter-a262c.firebaseapp.com",
+    databaseURL: "https://nwitter-a262c.firebaseio.com",
+    projectId: "nwitter-a262c",
+    storageBucket: "nwitter-a262c.appspot.com",
+    messagingSenderId: "931613634780",
+    appId: "1:931613634780:web:89591311c0553179514419"
+};
+
+export default firebase.initializeApp(firebaseConfig);
