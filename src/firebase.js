327083334e23399164adb1ac3a6c2af1fd000e20
Charles An <mucollabo@gmail.com>
2020-09-21 23:20:18 +0900

    study 1.2 router setup

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@
 import * as firebase from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC0Vlut07MqjGgkXcxjrS0uCjv-c9f9GLs",
-    authDomain: "nwitter-a262c.firebaseapp.com",
-    databaseURL: "https://nwitter-a262c.firebaseio.com",
-    projectId: "nwitter-a262c",
-    storageBucket: "nwitter-a262c.appspot.com",
-    messagingSenderId: "931613634780",
-    appId: "1:931613634780:web:89591311c0553179514419"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+    appId: process.env.REACT_APP_APP_ID
 };
 
 export default firebase.initializeApp(firebaseConfig);
