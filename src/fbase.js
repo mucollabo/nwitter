cca7e6ea092c3a87ee745ff85a68250f1cb7c7f3
Charles An <mucollabo@gmail.com>
2020-09-24 20:01:33 +0900

    study 2.1 Login...

--- src/fbase.js
+++ src/fbase.js
@@ -1,5 +1,5 @@
-
 import * as firebase from "firebase/app";
+import "firebase/auth";
 
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_API_KEY,
@@ -11,4 +11,6 @@ const firebaseConfig = {
     appId: process.env.REACT_APP_APP_ID
 };
 
-export default firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
+
+export const authService = firebase.auth();
