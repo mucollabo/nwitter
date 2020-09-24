631a7c1694ac2b161faf973d1f5303a841347b8a
Charles An <mucollabo@gmail.com>
2020-09-24 23:59:41 +0900

    study 2.3 Creating Account

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,8 +1,10 @@
 import React, {useState} from "react";
+import {authService} from "../fbase";
 
 const Auth = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [newAccount, setNewAccount] = useState(true);
     const onChange = (event) => {
         const {
             target: {name, value},
@@ -13,15 +15,32 @@ const Auth = () => {
             setPassword(value);
         }
     };
-    const onSubmit = (event) => {
+    const onSubmit = async(event) => {
         event.preventDefault();
+        try {
+            let data;
+            if(newAccount) {
+                data = await authService.createUserWithEmailAndPassword(
+                    email,
+                    password
+                );
+            } else {
+                data = await authService.signInWithEmailAndPassword(
+                    email,
+                    password
+                );
+            }
+            console.log(data)
+        } catch(error) {
+            console.log(error);
+        }
     };
     return (
         <div>
             <form onSubmit={onSubmit}>
                 <input
                     name="email"
-                    type="text"
+                    type="email"
                     placeholder="Email"
                     required
                     value={email}
@@ -37,7 +56,7 @@ const Auth = () => {
                 />
                 <input
                     type="submit"
-                    value="Log In"
+                    value={newAccount ? "Create Account" : "Log In"}
                 />
             </form>
             <div>
